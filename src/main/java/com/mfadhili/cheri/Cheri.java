d13496c079bb2cacb4d72ea84f4c1e781921b6cc
mfadhili <mfadhili@github.com>
2022-12-12 10:59:57 +0300

    testing war deployment

--- src/main/java/com/mfadhili/cheri/Cheri.java
+++ src/main/java/com/mfadhili/cheri/Cheri.java
@@ -8,7 +8,7 @@ public class Cheri {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cheri.class, args);
-		System.out.print("For API documentation check: http://localhost:8081/swagger-ui/");
+		System.out.print("For API documentation check: http://localhost:3081/swagger-ui/");
 	}
 
 
